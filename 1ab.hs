83e155b362edfa16e41ed0ed066360e3dac5e5f2
Lorenzo <lollobaldo2000@gmail.com>
2019-02-01 19:34:04 +0000

    1ab

--- 1ab.hs
+++ 1ab.hs
@@ -8,17 +8,31 @@ mainA = do
 
 mainB = do
           contents <- readFile "1ab.txt"
-          putStrLn contents
+          putStrLn $ execB $ parse contents
 
+--parse by line based on '\n' Char
+--positive Ints cant have '+' sign
 parse :: String -> [Int]
-parse str = map readNeg $ splitOn "\n" str :: [Int]
-              where
-                readNeg :: String -> Int
-                readNeg ('+':ns) = read ns
-                readNeg ns = read ns
-
-myFunc :: String -> Int
-myFunc s = read s
+parse str = map read $ splitOn "\n" $ filter (/= '+') str :: [Int]
 
 execA :: [Int] -> String
-execA ns = show $ foldr (+) 0 ns
+execA ns = show $ sum ns
+
+execB :: [Int] -> String
+execB ns = show . firstDuplicate . partialSum . cycle $ ns
+
+partialSum' :: [Int] -> [Int]
+partialSum' = scanl (+) 0
+
+partialSum :: [Int] -> [Int]
+partialSum (lastFreqs:firstChange:changes) = sum : partialSum (sum:changes)
+                                        where
+                                          sum = lastFreqs + firstChange
+
+firstDuplicate :: [Int] -> Int
+firstDuplicate = helperDuplicate []
+
+helperDuplicate :: [Int] -> [Int] -> Int
+helperDuplicate prevs (n:ns)
+                              | n `elem` prevs = n
+                              | otherwise = helperDuplicate (prevs ++ [n]) ns
