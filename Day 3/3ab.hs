556adf608bce5ac6f170b853002657d3aede3014
Lorenzo <lollobaldo2000@gmail.com>
2019-02-04 16:40:48 +0000

    adapted data representation for (b)

--- Day 3/3ab.hs
+++ Day 3/3ab.hs
@@ -1,8 +1,10 @@
 module Main where
 import Data.List.Split
+import Data.Char (isDigit)
 
 type Fabric = [[SquareInch]]
-type Piece = (Int, Int, Int, Int)
+type ID = Int
+type Piece = (ID, Int, Int, Int, Int)
 type Pieces = [Piece]
 
 data SquareInch = Free | Taken | Conflict deriving (Eq,Show)
@@ -15,7 +17,10 @@ main = do
 
 --parse by line based on '\n' Char
 parse :: String -> [Piece]
-parse str = [(read x,read y,read a,read b) | [_,_,_,x,y,_,a,b] <- map (splitOneOf "@:,x ") $ lines str]
+parse str = [(id, x, y, a, b) | [id,x,y,a,b] <- map (map read . filter isNumber . splitOneOf "# @,:x") $ lines str]
+
+isNumber :: String -> Bool
+isNumber str = (all isDigit) str && (not . null) str
 
 -- parserHelper :: [String] -> Piece
 -- parserHelper [_,_,_,x,y,_,a,b] = ()
@@ -30,7 +35,7 @@ fabric :: Fabric
 fabric = replicate 1001 $ replicate 1001 Free
 
 insert :: Fabric -> Piece -> Fabric
-insert fab (x,y,w,h) = [if not (i > y && i <= (y+h))
+insert fab (_,x,y,w,h) = [if not (i > y && i <= (y+h))
                           then row
                           else
                             [if not (j > x && j <= (x+w))
@@ -47,3 +52,19 @@ final fab (p:ps) = final (insert fab p) ps
 
 countConflicts :: Pieces -> Int
 countConflicts ps = length [x | x <- (concat (final fabric ps)) , x == Conflict]
+
+-- final :: Pieces -> Pieces -> Int
+-- final _ [] = 0
+-- final pre (p@(id,_,_,_,_):ps)
+--               | traceShow (p) (check p (pre++ps)) = id
+--               | otherwise = final (pre++[p]) ps
+
+-- formatter ps = [format p | p <- ps]
+-- format (id,x,y,w,h) = (id,x+1,y+1,x+w,y+h)
+
+-- check :: Piece -> Pieces -> Bool
+-- check p@(_,x,y,w,h) ps = and [notIntersect p s | s <-ps]
+
+-- notIntersect :: Piece -> Piece -> Bool
+-- notIntersect (_,x,y,w,h) (_,a,b,c,d) = (x > c || a > w ||
+--                                         y > d || b > h)
