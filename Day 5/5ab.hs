35945f34eba66ba7b15ecb1f332325efaeeccd7b
Lorenzo <lollobaldo2000@gmail.com>
2019-02-04 18:38:55 +0000

    some 5ab

--- Day 5/5ab.hs
+++ Day 5/5ab.hs
@@ -1,19 +1,52 @@
 module Main where
-import Data.List.Split
-import Data.Char (isDigit)
+
+import Data.Char
+
+type Unit = Char
+type Polymer = [Unit]
 
 main = do
-          contents <- readFile "3ab.txt"
-          --print $ parse contents
-          putStrLn $ execA $ parse contents
-          putStrLn $ execB $ parse contents
+          contents <- readFile "5ab.txt"
+          --print $ length $ reduce contents
+          --print $ foldl1 min $ (\c -> length $ reduce $ filter (\d -> (toLower d) /= c) contents) <$> ['a'..'z']
+          --print $ execA $ parse contents
+          --print $ execB $ parse contents
 
 --parse by line based on '\n' Char
-parse :: String -> [Piece]
-parse str = [(id, x, y, a, b) | [id,x,y,a,b] <- map (map read . filter isNumber . splitOneOf "# @,:x") $ lines str]
+parse :: String -> Polymer
+parse str = str
 
-execA :: [Piece] -> String
-execA = show . countConflicts
+execA :: Polymer -> Int
+execA = length . whileChanges reactor
 
-execB :: [Piece] -> String
-execB = show . checkFree [] . formatter
+execB :: Polymer -> Int
+execB = minimum . tryAll ['a'..'z']
+
+reactor :: Polymer -> Polymer
+reactor [] = []
+reactor [a] = [a]
+reactor (x:y:zs)
+                | checkIfReact x y = reactor zs
+                | otherwise        = x : reactor (y:zs)
+
+checkIfReact :: Unit -> Unit -> Bool
+checkIfReact a b = (toLower a == toLower b) && isUpper a /= isUpper b
+
+whileChanges :: Eq a => (a -> a) -> a -> a
+whileChanges f str
+                    | str == new = str
+                    | otherwise  = whileChanges f new
+                        where
+                          new = f str
+
+tryAll :: String -> String -> [Int]
+tryAll (ch:alph) str = [length . whileChanges reactor . remover ch $ str | ch <- str]
+
+remover :: Char -> String -> String
+remover ch str = filter ((ch /=) . toLower) str
+
+reduce l = foldl reduce' [] l
+    where reduce' [] x = [x]
+          reduce' acc@(y:_) x
+            | x /= y && (toLower x) == (toLower y) = tail acc
+            | otherwise                            = x:acc
