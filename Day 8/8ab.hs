4361e7f2dad072856a6966c9b492abe73b8de1a2
Lorenzo <lollobaldo2000@gmail.com>
2019-02-08 01:12:24 +0000

    no clue yet

--- Day 8/8ab.hs
+++ Day 8/8ab.hs
@@ -1,46 +1,33 @@
 module Main where
 
-import Data.List.Split
+--import Data.List.Split
 import Data.List
 import Data.Char (isUpper)
 
-type Clause = (Instruction , Instruction)
-type Requirement = (Instruction , [Instruction])
-type Instruction = Char
+data Node = Node [Node] [Int] deriving (Show)
 
 main = do
-          contents <- readFile "7ab.txt"
-          print . execA . parse $ contents
+          contents <- readFile "8ab.txt"
+          print . parse $ contents
+          --print . execA . parse $ contents
           --print . execB . parse $ contents
 
 --parse by line based on '\n' Char
-parse :: String -> [Clause]
-parse str = [(a, b) | [a,b] <- map (tail . filter isUpper) $ lines str]
+parse :: String -> Node
+parse str = populateTree $ map read $ words str
 
-execA :: [Clause] -> String
-execA = satisfy [] . putTogether
+execA :: [Int] -> Int
+execA = undefined
 
-execB :: [Clause] -> String
+execB :: [Int] -> String
 execB = undefined
 
-emptyReqs :: [Requirement]
-emptyReqs = zip ['A'..'Z'] (cycle [[]])
-
-putTogether :: [Clause] -> [Requirement]
-putTogether = foldl' (\reqs cl -> insertInReqs cl reqs) emptyReqs
-
-insertInReqs :: Clause -> [Requirement] -> [Requirement]
-insertInReqs (a,b) (cl@(c,str):reqs)
-                                      | c == b    = (c,a:str):reqs
-                                      | otherwise = cl : insertInReqs (a,b) reqs
-
-removeFromReqs :: Instruction -> Requirement -> Requirement
-removeFromReqs ch req@(c,str)
-                              | ch `elem` str = (c,str\\[ch])
-                              | otherwise     = req
-
-satisfy :: [Requirement] -> [Requirement] -> String
-satisfy _ [] = ""
-satisfy acc (req@(c,str):reqs)
-                              | str == "" = c : satisfy [] (map (removeFromReqs c) (acc ++ reqs))
-                              | otherwise = satisfy (acc ++ [req]) reqs
+populateTree :: [Int] -> Node
+populateTree [] = Node [] []
+populateTree (nc:nm:xs) = Node [populateTree childs] metas
+                            where
+                              childa 0 x = []
+                              childs n x = parse x : (childa (n-1) (drop (size $ parse x) x))
+                              --childs = fst splitted
+                              metas  = snd splitted
+                              splitted = splitAt (length xs - nm) xs
