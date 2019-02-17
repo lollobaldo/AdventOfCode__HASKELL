9486802d8b39b0b37905336f3df5d8e2857b869c
Lorenzo <lollobaldo2000@gmail.com>
2019-02-17 00:01:53 +0000

    8ab

--- Day 8/8ab.hs
+++ Day 8/8ab.hs
@@ -8,26 +8,41 @@ data Node = Node [Node] [Int] deriving (Show)
 
 main = do
           contents <- readFile "8ab.txt"
-          print . parse $ contents
-          --print . execA . parse $ contents
-          --print . execB . parse $ contents
+          --print . parse $ contents
+          print . execA . parse $ contents
+          print . execB . parse $ contents
 
 --parse by line based on '\n' Char
 parse :: String -> Node
 parse str = populateTree $ map read $ words str
 
-execA :: [Int] -> Int
-execA = undefined
+execA :: Node -> Int
+execA = sumMetas
 
-execB :: [Int] -> String
-execB = undefined
+execB :: Node -> Int
+execB = getValue
+
+sumMetas :: Node -> Int
+sumMetas (Node childs metas) = sum metas + sum (map sumMetas childs)
+
+nodeLength :: Node -> Int
+nodeLength (Node childs metas) = 2 + nodeLength' childs + length metas
+nodeLength' = sum . map nodeLength
+
+getNthChild :: [Node] -> Int -> Maybe Node
+getNthChild _      0 = Nothing
+getNthChild []     _ = Nothing
+getNthChild (x:_)  1 = Just x
+getNthChild (_:xs) i = getNthChild xs (i-1)
+
+getValue :: Node -> Int
+getValue (Node [] metas) = sum metas
+getValue (Node childs metas) = sum . map (maybe 0 getValue . getNthChild childs) $ metas
 
 populateTree :: [Int] -> Node
 populateTree [] = Node [] []
-populateTree (nc:nm:xs) = Node [populateTree childs] metas
+populateTree (nc:nm:xs) = Node (childs nc xs) metas
                             where
-                              childa 0 x = []
-                              childs n x = parse x : (childa (n-1) (drop (size $ parse x) x))
-                              --childs = fst splitted
-                              metas  = snd splitted
-                              splitted = splitAt (length xs - nm) xs
+                              childs 0          xs = []
+                              childs childsLeft xs = populateTree xs : childs (childsLeft - 1) (drop (nodeLength $ populateTree xs) xs)
+                              metas = take nm $ drop (nodeLength' $ childs nc xs) xs
