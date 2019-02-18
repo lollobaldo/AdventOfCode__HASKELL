e8a7faa2d586767affeefab6d6f6ed2955946075
Lorenzo <lollobaldo2000@gmail.com>
2019-02-18 00:22:15 +0000

    working 9a

--- Day 9/9.hs
+++ Day 9/9.hs
@@ -1,10 +1,11 @@
 module Main where
 
-import Data.List.Split
 import Data.List
 import Data.Char (isUpper, isDigit)
+import Debug.Trace
 
-type Point = (Int, Int, Int, Int)
+type Points = Int
+type Score  = (Int,Points)
 
 main = do
           contents <- readFile "9.txt"
@@ -12,14 +13,48 @@ main = do
           --print . execB . parse $ contents
 
 --parse by line based on '\n' Char
-parse :: String -> [Point]
-parse str = [(x, y, u, v) | [x,y,u,v] <- map (map read . filter isNumber . splitOneOf "<,>") $ lines str]
+parse :: String -> (Int, Points)
+parse str = (players, rocks)
+              where
+                tup = map read $ filter isNumber . words $ str
+                players = head tup
+                rocks   = last tup
 
 isNumber :: String -> Bool
 isNumber str = (all (`elem` " +-0123456789")) str && (not . null) str
 
-execA :: [Point] -> [Point]
-execA = id
+execA :: (Int, Points) -> Int
+execA = winner . play
+            where
+              play (p, n) = playGame (n+1) 1 p [0] (zip [0..p-1] (repeat 0))
 
-execB :: [Point] -> String
+execB :: (Int, Points) -> String
 execB = undefined
+
+winner :: [Score] -> Int
+winner = maximum . map snd
+
+playGame :: Int -> Int -> Int -> [Int] -> [Score] -> [Score]
+playGame 0    _ _ _                      ret = ret
+playGame left 1 p [0]                   score = playGame (left-1) 2 p [1,0] score
+playGame left 2 p [1,0]                 score = playGame (left-1) 3 p [2,1,0] score
+playGame left n p game@(current:one:xs) score
+                                                -- | n `mod` 23 == 0 = playGame (left-1) (n+1) p (trace (show newGame) newGame) (addPoints (n `mod` p) (n+addn) score)
+                                                -- | otherwise       = playGame (left-1) (n+1) p (trace (show (n:xs ++ [current,one])) (n:xs ++ [current,one])) score
+                                                | n `mod` 23 == 0 = playGame (left-1) (n+1) p newGame (addPoints (n `mod` p) (n+addn) score)
+                                                | otherwise       = playGame (left-1) (n+1) p (n:xs ++ [current,one]) score
+                                                    where
+                                                      addn     = (head . snd) split7
+                                                      newGame  = newStart ++ newEnd
+                                                      newEnd   = fst split7
+                                                      newStart = (tail . snd) split7
+                                                      split7 :: ([Int], [Int])
+                                                      split7 = splitAt (length game - 7) game
+
+addPoints :: Int -> Points -> [Score] -> [Score]
+addPoints player points scores = addPoints' player points [] scores --trace ("Add " ++ show points ++ " points to player " ++ show player) addPoints' player points [] scores
+
+addPoints' :: Int -> Points -> [Score] -> [Score] -> [Score]
+addPoints' player points acc ((p,s):scores)
+                                          | player == p = acc ++ ((p,s+points):scores)
+                                          | otherwise   = addPoints' player points (acc ++ [(p,s)]) scores
